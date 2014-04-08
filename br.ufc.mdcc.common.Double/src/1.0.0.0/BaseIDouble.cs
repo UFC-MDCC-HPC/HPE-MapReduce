b717c2625f62ce140e0eb4b33cc74c96baacdb4b
Cenez <cenezaraujo@gmail.com>
2014-04-08 01:23:38 -0300

    Creating IDouble component

---
+++ br.ufc.mdcc.common.Double/src/1.0.0.0/BaseIDouble.cs
@@ -0,0 +1,15 @@
+/* AUTOMATICALLY GENERATE CODE */
+
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Data;
+
+namespace br.ufc.mdcc.common.Double { 
+
+public interface BaseIDouble : BaseIData, IDataStructureKind 
+{
+
+
+
+} // end main interface 
+
+} // end namespace 
---
+++ br.ufc.mdcc.common.Double/src/1.0.0.0/IDouble.cs
@@ -0,0 +1,13 @@
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Data;
+
+namespace br.ufc.mdcc.common.Double { 
+
+public interface IDouble : BaseIDouble, IData
+{
+
+		int Value { get; set; }
+
+} // end main interface 
+
+} // end namespace 
