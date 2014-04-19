256b6c197158fa8079818521ab36619ac00f104d
Cenez <cenezaraujo@gmail.com>
2014-04-19 17:10:36 -0300

    Add PageRankApp/PageRankAppImpl

--- br.ufc.mdcc.mapreduce.example.graph.pagerank.impl.PageRankAppImpl/src/1.0.0.0/IMasterProcessImpl.cs
+++ br.ufc.mdcc.mapreduce.example.graph.pagerank.impl.PageRankAppImpl/src/1.0.0.0/IMasterProcessImpl.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using br.ufc.pargo.hpe.backend.DGAC;
 using br.ufc.pargo.hpe.basic;
 using br.ufc.pargo.hpe.kinds;
@@ -16,7 +17,7 @@ namespace br.ufc.mdcc.mapreduce.example.graph.pagerank.impl.PageRankAppImpl {
 		public IMasterProcessImpl() { } 
 
 		public override void main() {
-			Input_data.Value = readInput();
+			Input_data.Value = readInput("pathFile");
 			Page_rank.go();
 			while (!Output_data.HasFinished) {
 				IKVPair<IInteger,IDouble> kv = Output_data.fetch_next();
@@ -25,8 +26,37 @@ namespace br.ufc.mdcc.mapreduce.example.graph.pagerank.impl.PageRankAppImpl {
 			}
 		}
 
-		string readInput (){
-			throw new NotImplementedException ();
+		string readInput(string path){
+			string str = "";
+			byte[] dataByte = readFile (path);
+			foreach (byte b in dataByte) {
+				str = str+Convert.ToChar(b);
+			}
+			return str;
+		}
+		byte[] readFile(string path) {
+			string PATH = @path;
+			byte[] res = null;
+			try{
+				using (FileStream fileread = new FileStream(PATH, FileMode.Open, FileAccess.Read)){					
+					byte[] bytes = new byte[fileread.Length];
+					int bytesToRead = (int)fileread.Length-1;
+					int bytesRead = 0;
+					while (bytesToRead > 0){
+						int n = fileread.Read(bytes, bytesRead, bytesToRead);
+						if (n == 0)
+							break;
+						bytesRead += n;
+						bytesToRead -= n;
+					}
+					bytesToRead = bytes.Length;
+					res = bytes;
+				}
+			}
+			catch (FileNotFoundException e){
+				Console.WriteLine("Exception IMasterProcessImpl readFile method: "+e.Message);
+			}
+			return res;
 		}
 	}
 }
