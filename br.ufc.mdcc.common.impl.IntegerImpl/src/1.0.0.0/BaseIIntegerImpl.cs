931d11ca828253b84ed9a5f7d6df421f9ac4d8a1
heron <heron@heron-Studio-XPS-1340.(none)>
2014-05-18 18:53:37 -0300

    Map-Reduce / Word Count OK

---
+++ br.ufc.mdcc.common.impl.IntegerImpl/src/1.0.0.0/BaseIIntegerImpl.cs
@@ -0,0 +1,18 @@
+/* Automatically Generated Code */
+
+using System;
+using br.ufc.pargo.hpe.backend.DGAC;
+using br.ufc.pargo.hpe.basic;
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Integer;
+
+namespace br.ufc.mdcc.common.impl.IntegerImpl { 
+
+	public abstract class BaseIIntegerImpl: DataStructure, BaseIInteger
+{
+
+
+
+}
+
+}
---
+++ br.ufc.mdcc.common.impl.IntegerImpl/src/1.0.0.0/IIntegerImpl.cs
@@ -0,0 +1,55 @@
+using System;
+using br.ufc.pargo.hpe.backend.DGAC;
+using br.ufc.pargo.hpe.basic;
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.common.Integer;
+using br.ufc.mdcc.common.Data;
+using System.Collections.Generic;
+
+namespace br.ufc.mdcc.common.impl.IntegerImpl 
+{ 
+
+	public class IIntegerImpl : BaseIIntegerImpl, IInteger
+	{
+
+		public IIntegerImpl() { } 
+
+		public IIntegerInstance newInstance (int i)
+		{
+			IIntegerInstance instance = (IIntegerInstance)newInstance ();
+			instance.Value = i;
+			return instance;
+		}
+
+		public object newInstance ()
+		{
+			this.instance = new IIntegerInstanceImpl ();
+			return this.Instance;
+		}
+
+		private IIntegerInstance instance;
+
+		public object Instance {
+			get { return instance;	}
+			set { this.instance = (IIntegerInstance) value; }
+		}
+	}
+
+	public class IIntegerInstanceImpl : IIntegerInstance
+	{
+		#region IIntegerInstance implementation
+
+		private int val;
+
+		public int Value {
+			get { return val; }
+			set { this.val = value;	}
+		}
+
+		#endregion
+
+
+	}
+
+
+}
