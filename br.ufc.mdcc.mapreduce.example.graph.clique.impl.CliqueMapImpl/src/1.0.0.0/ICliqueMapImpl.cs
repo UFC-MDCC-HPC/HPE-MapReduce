5ab21bb234b186ae2e9a7650ae16e229a5d69d36
Cenez <cenezaraujo@gmail.com>
2014-07-14 20:09:31 -0300

    Clique Update to Instances

--- br.ufc.mdcc.mapreduce.example.graph.clique.impl.CliqueMapImpl/src/1.0.0.0/ICliqueMapImpl.cs
+++ br.ufc.mdcc.mapreduce.example.graph.clique.impl.CliqueMapImpl/src/1.0.0.0/ICliqueMapImpl.cs
@@ -6,7 +6,6 @@ using br.ufc.mdcc.common.Iterator;
 using br.ufc.mdcc.common.KVPair;
 using br.ufc.mdcc.common.Data;
 using br.ufc.mdcc.common.Integer;
-using br.ufc.mdcc.common.impl.KVPairImpl;
 using br.ufc.mdcc.mapreduce.example.graph.clique.CliqueNode;
 using br.ufc.mdcc.mapreduce.example.graph.clique.CliqueMap;
 
