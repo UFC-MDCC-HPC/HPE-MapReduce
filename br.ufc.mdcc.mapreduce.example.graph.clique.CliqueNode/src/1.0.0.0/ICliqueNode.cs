17cc2bc03020d7ae593e041a871c7b7ec26546f7
Cenez Araujo de Rezende <cenezaraujo@egmail.com>
2014-10-27 19:44:48 -0300

    =

--- br.ufc.mdcc.mapreduce.example.graph.clique.CliqueNode/src/1.0.0.0/ICliqueNode.cs
+++ br.ufc.mdcc.mapreduce.example.graph.clique.CliqueNode/src/1.0.0.0/ICliqueNode.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using br.ufc.pargo.hpe.kinds;
 using br.ufc.mdcc.common.graph.DataNode;
+using System;
 
 namespace br.ufc.mdcc.mapreduce.example.graph.clique.CliqueNode { 
 	public interface ICliqueNode : BaseICliqueNode, IDataNode{
 		object newInstance (int _id);
 	} // end main interface 
-	public interface ICliqueNodeInstance{
+	public interface ICliqueNodeInstance: ICloneable{
 		int IdInstance { get; set;}
 		IList<int> NeighborsInstance { set; get; }
 	}
